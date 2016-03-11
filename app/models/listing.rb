584cad0857a174ba49ba62376833b263ba08ce57
Duy Huynh <dhuynh88@uw.edu>
2016-03-11 07:27:36 -0800

    Listings now have image attachment.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,9 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image
+
+  #If you want to require images in listing
+  # validates :image, presence: true
+
+
+  has_attached_file :image, styles: {:medium => '640x'}
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
