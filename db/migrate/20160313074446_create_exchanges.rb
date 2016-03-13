2ed32a1946007ab688b5242a06dc28a1c3a8aac1
Henry Lawrence <hlawrence6809@gmail.com>
2016-03-13 12:11:35 -0700

    Fixed exchanges migration

--- db/migrate/20160313074446_create_exchanges.rb
+++ db/migrate/20160313074446_create_exchanges.rb
@@ -1,9 +1,10 @@
 class CreateExchanges < ActiveRecord::Migration
   def change
     create_table :exchanges, id: false do |t|
-      t.primary_key :exchange_id, null: false
+      t.primary_key :exchange_id
       t.foreign_key :listings_id, null: false
       t.date :borrowed_on,        null: false
+      t.date :expected_return,    null: false
       t.date :returned_on
       t.foreign_key :borrower_id, null: false
       t.foreign_key :lender_id,   null: false
