50195c5be8e58a3a8124da54f1e1fa346da4d550
Duy Huynh <dhuynh88@uw.edu>
2016-03-13 16:55:33 -0700

    Added admin role. This will be used in the future to delete users and anyone's listings.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313184024) do
+ActiveRecord::Schema.define(version: 20160313233928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,10 +47,11 @@ ActiveRecord::Schema.define(version: 20160313184024) do
     t.string   "state"
     t.string   "zip"
     t.string   "phone"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,16 @@ Listing.create(listing_id: 1, title: 'Item 1', owner_id: 1, borrow_length: 4, da
 Listing.create(listing_id: 2, title: 'Item 2', owner_id: 2, borrow_length: 5, date_created: '2016-02-13', category: 'Items', description: 'This is item 2')
 Listing.create(listing_id: 3, title: 'Item 3', owner_id: 3, borrow_length: 6, date_created: '2016-02-17', category: 'Items', description: 'This is item 3')
 
-User.create(name: 'User 1', address: '1234 E Street', city: 'Tacoma', state: 'Washington', zip: '984xx', phone: '253-xxx-xxx', email: 'user@email.com', password: 'swapr')
+User.create(name: 'Admin',
+            address: '1234 Admin Street',
+            city: 'Administration',
+            state: 'Washington',
+            zip: '11111',
+            phone: '999-999-9999',
+            email: 'admin@example.com',
+            password: 'adminpassword',
+            password_confirmation: 'adminpassword',
+            admin: true)
 
 # Create some fake users:
 20.times do |user|
