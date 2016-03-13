e5b43fe67c19616a6c22839343b0027215122292
Henry Lawrence <hlawrence6809@gmail.com>
2016-03-13 12:13:26 -0700

    Fixed migrate issues

--- db/migrate/20160313074446_create_exchanges.rb
+++ db/migrate/20160313074446_create_exchanges.rb
@@ -2,12 +2,12 @@ class CreateExchanges < ActiveRecord::Migration
   def change
     create_table :exchanges, id: false do |t|
       t.primary_key :exchange_id
-      t.foreign_key :listings_id, null: false
+      t.integer :listings_id,     null: false
       t.date :borrowed_on,        null: false
       t.date :expected_return,    null: false
       t.date :returned_on
-      t.foreign_key :borrower_id, null: false
-      t.foreign_key :lender_id,   null: false
+      t.integer :borrower_id,     null: false
+      t.integer :lender_id,       null: false
     end
   end
 end
