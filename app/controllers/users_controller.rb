0b518cfcc628ecbc88961ec3580ffa434f54122a
Duy Huynh <dhuynh88@uw.edu>
2016-03-13 01:32:39 -0800

    Signups are working and create Users in the model/db. Only name and email at the moment.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,31 @@
 class UsersController < ApplicationController
 
+  #Display User info
   def show
     @user = User.find(params[:id])
   end
 
+  # What to do when creating a new user (make a new User in db)
   def new
     @user = User.new
   end
+
+  # Creating new Users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to SWAPR!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  # Define private variables
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
