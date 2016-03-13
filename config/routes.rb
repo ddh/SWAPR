1ac03a4b2cbc7bb5edc5fc19ddf47775e7e3ff95
Duy Huynh <dhuynh88@uw.edu>
2016-03-12 18:44:08 -0800

    Added static pages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/...'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,7 +56,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   resources :listings
-  root 'welcome#index'
+  root 'listings#index'
 
   get 'listings/search/:query' => 'listings#search'
 
